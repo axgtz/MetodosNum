7962a00ce57c602b87df986fd7d011ac9767d8fa
axgtz <axguti@gmail.com>
2019-09-07 13:48:17 -0500

    fixed gauss simple when reading isntead of fixed

--- GaussSimple.m
+++ GaussSimple.m
@@ -6,9 +6,12 @@ clear
 
 fprintf('Introducir los valores del sistema de ecuaciones en una matriz entre corchete [], usar ; para separar fila y un espacio para columna\n');
 fprintf('\tEj. [2 3 1 0; 1 1 2 1; 1 -1 -1 -1]\n\n');
-matrix = input("Introduzca valores de matriz de 3x4: ");
+%matrix = input("Introduzca valores de matriz de 3x4: ");
 %matrix = [2 3 1 0; 1 1 2 1; 1 -1 -1 -1];   %Matriz prueba 1
 %matrix = [2 -1 1 2; 3 1 -2 9; -1 2 5 -5];  %Matriz prueba 2
+%matrix = [2 -1 3 5; 2 2 3 7; -2 3 0 -3];
+%matrix = [0 1 3 4;1 1 1 3; 0 2 3 5];
+matrix = [1 1 2 7; 0 2 -3 -2; 3 -1 3 7];
 
     %Version 3 - Mas generica
 if det(matrix(:, [1 2 3])) ~= 0 %Checar que el determinante no sea 0 para poder resolver, sin contar valores independientes
