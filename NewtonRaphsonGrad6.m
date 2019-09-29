c3d638016b0877e7b9c74ad0331939cbf1a16a54
axgtz <axguti@gmail.com>
2019-09-29 16:28:20 -0500

    added biseccion

--- NewtonRaphsonGrad6.m
+++ NewtonRaphsonGrad6.m
@@ -3,13 +3,14 @@ clear
 clc
 %ecuation = @(x0) 2*x0^2-2*x0-12; %anon function
 i = 1; % contador de iteraciones
+c0= input("Introduzca el valor de coeficiente ind= ");
 c1= input("Introduzca el valor de coeficiente 1= ");
 c2= input("Introduzca el valor de coeficiente 2= ");
 c3= input("Introduzca el valor de coeficiente 3= ");
 c4= input("Introduzca el valor de coeficiente 4= ");
 c5= input("Introduzca el valor de coeficiente 5= ");
 c6= input("Introduzca el valor de coeficiente 6= ");
-ecuation = @(x0) c6*x0^6+c5*x0^5+c4*x0^4+c3*x0^3+c2*x0^2+c1*x0-12; %anon function
+ecuation = @(x0) c6*x0^6+c5*x0^5+c4*x0^4+c3*x0^3+c2*x0^2+c1*x0+c0; %anon function
 
 x0= input("Introduzca el valor de x0= ");
 error = 20;
